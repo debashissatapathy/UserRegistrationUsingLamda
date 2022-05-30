722f31affd851b50a054bdaf516434e57b277e59
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-30 10:38:08 +0530

    User registration using lambda

---
+++ UserRegistrationUsingLambda/Program.cs
@@ -0,0 +1,102 @@
+﻿using UserRegistrationUsingLambda;
+using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        UserRegistration UR = new UserRegistration();
+        Console.WriteLine("Enter first name");
+        string FirstName = Console.ReadLine();
+        string check = UR.FirstNameUsingLambda(FirstName);
+        if (check == "First Name is valid")
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(check);
+            Console.ResetColor();
+
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(check);
+            Console.ResetColor();
+        }
+        
+        Console.WriteLine("Enter last name");
+        string LastName = Console.ReadLine();
+        string check2 = UR.LastNameUsingLambda(LastName);
+        if (check2 == "Last Name is valid")
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(check2);
+            Console.ResetColor();
+
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(check2);
+            Console.ResetColor();
+        }
+        Console.WriteLine("Enter your Email id");
+        string Email = Console.ReadLine();
+        string check3 = UR.EmailusingLambda(Email);
+        if (check3 == "Email is valid")
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(check3);
+            Console.ResetColor();
+
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(check3);
+            Console.ResetColor();
+        }
+        Console.WriteLine("Enter your Phone Number");
+        string PhoneNumber = Console.ReadLine();
+        string check4 = UR.PhoneNumberUsingLmbda(PhoneNumber);
+        if (check4 == "Phone number is valid")
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(check4);
+            Console.ResetColor();
+
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(check4);
+            Console.ResetColor();
+        }
+        Console.WriteLine("Enter Password");
+        string Password = Console.ReadLine();
+        string check5 = UR.PasswordUsingLambda(Password);
+        if (check5 == "Password is valid")
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(check5);
+            Console.ResetColor();
+
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(check5);
+            Console.ResetColor();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+   
+
---
+++ UserRegistrationUsingLambda/UserRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    public class UserRegistration
+    {
+        string FirstName = @"[A-Z]{1}[a-z]{2,}$";
+        string LastName = @"[A-Z]{1}[a-z]{2,}$";
+        string Email_Id = @"^[A-Za-z0-9]+.[A-Za-z0-9]+@[A-Za-z0-9]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        string PhoneNumber = @"[0-9]{2}[ ]{1}[6-9]{1}[0-9]{9}";
+        string Password = "^(?=.*[@#$%0-9A-Z])[@#$%0-9a-zA-Z]{8,}$";
+        public string FirstNameUsingLambda(string firstname) => Regex.IsMatch(firstname, FirstName) ? "First Name is valid" : "Invalid First Name";
+        public string LastNameUsingLambda(string lastname) => Regex.IsMatch(lastname, LastName) ? "Last Name is valid" : "Invalid Last Name";
+
+        public string EmailusingLambda(string email) => Regex.IsMatch(email, Email_Id) ? "Email is Valid" : "Invalid Email";
+        public string PhoneNumberUsingLmbda(string phonenumber) => Regex.IsMatch(phonenumber, PhoneNumber) ? "Phone number is valid" : "Invalid Phone number";
+        public string PasswordUsingLambda(string password) => Regex.IsMatch(password, Password) ? "Password is Valid!" : "Invallid Password";
+
+
+    }
+}
